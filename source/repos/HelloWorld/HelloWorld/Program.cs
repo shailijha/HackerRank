14ceefbe09eb17fb230de231d6816726e78462b5
shailijha <shaili_jha@yahoo.co.in>
2019-09-13 19:54:39 -0400

    Committing Mosh Beginner files

---
+++ source/repos/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            //const float Pi = 3.14f;
+
+            int i = 1000;
+            byte b = (byte) i;
+            Console.WriteLine(b);
+
+            string number = "1224";
+            int a = Convert.ToInt32(number);
+            byte b1 = Convert.ToByte(number);
+            Console.WriteLine(a);
+            Console.WriteLine(b1);
+        }
+    }
+}
